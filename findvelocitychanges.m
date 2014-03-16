e99f9a70596aac4313c6f6c6b687bc588db1e67c
Graham Smith <grahamas@gmail.com>
2014-03-16 15:56:15 -0500

    Fixed dup (+10) indices in findvelocitychanges.m

--- findvelocitychanges.m
+++ findvelocitychanges.m
@@ -3,9 +3,13 @@ function [ velchangeindices ] = findvelocitychanges( cartvel, threshold, idxwind
 %   Detailed explanation goes here
 
 velchangeindices = [];
-for i=1:length(cartvel)-idxwindow
+i=1;
+while i < length(cartvel)-idxwindow
     if abs(cartvel(i) - cartvel(i + idxwindow)) > threshold
-        velchangeindices = [velchangeindices, i, i+idxwindow];
+        velchangeindices = [velchangeindices, i+(round(idxwindow/2))];
+        i = i+idxwindow;
+    else
+        i = i+1;
     end
 end
 
